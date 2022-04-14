2e6e74276f938cc80faf0ff7be9d2b18906e32d5
JyotiRSharma <appdeveloperubuntu@gmail.com>
2022-04-14 19:10:55 +0530

    created a todolist for work and home pages | added about.ejs

--- app.js
+++ app.js
@@ -4,35 +4,50 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+const items = ["Get food", "Buy food", "Eat food"];
+const workItems = [];
 
 app.get("/", (req, res) => {
     // res.send("express works!");
     let today = new Date();
-    let dayNumber = today.getDay();
-    let day = "";
+    let options = {
+        weekday: 'long',
+        day: '2-digit'
+    };
 
-    let weekdays = new Array(7);
-    weekdays[0] = "Sunday";
-    weekdays[1] = "Monday";
-    weekdays[2] = "Tuesday";
-    weekdays[3] = "Wednesday";
-    weekdays[4] = "Thursday";
-    weekdays[5] = "Friday";
-    weekdays[6] = "Saturday";
+    let day = today.toLocaleString("en-US", options);
 
-    if (dayNumber === 6 || dayNumber === 0) {
-        // res.sendFile(__dirname + "/weekend.html");
-        day = weekdays[dayNumber];
-        // console.log(day);
-    } else {
-        // res.sendFile(__dirname + "/weekday.html");
-        day = weekdays[dayNumber];
-        // console.log(day);
-    }
-
-    res.render("list", { kindOfDay: day });
+    res.render("list", { listTitle: day, newListItems: items });
 
 })
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work Items", newListItems: workItems });
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+app.post("/", (req, res) => {
+    let item = req.body.newItem; // get the new item.
+    let reqType = req.body.button; // Sniff on the button name
+
+    // let the hacking begin!
+
+    // Here we are dynamically collecting data from the button click nature
+    // the button is dynamically names with the "listTitle" variable in the "list" template.
+    if (reqType === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {       
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
 
 app.listen(3000, () => {
     console.log("serving on port 3000!");
